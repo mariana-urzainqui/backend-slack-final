993769856905b25b94c4f59d800d65b61cf5931d
mariana.urzainqui <marianaurzainqui@gmail.com>
2024-12-21 10:46:27 -0300

    cors

--- src/server.js
+++ src/server.js
@@ -19,7 +19,6 @@ app.use(cors({
     origin: ['https://frontend-slack-final.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
 }))
 app.use(express.json({ limit: '5mb' }))
 app.use(verifyApiKeyMiddleware)
