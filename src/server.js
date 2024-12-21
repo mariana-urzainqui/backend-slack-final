fc0265233e48f1121c2a9f4fdba220aaf6c90778
mariana.urzainqui <marianaurzainqui@gmail.com>
2024-12-21 10:54:04 -0300

    cors

--- src/server.js
+++ src/server.js
@@ -13,10 +13,10 @@ import memberRouter from "./router/member.router.js"
 
 const app = express()
 
-const cors = require('cors');
+const cors = require('cors')
 
 app.use(cors({
-    origin: 'https://frontend-slack-final.vercel.app'
+    origin: '*'
 }))
 
 app.use(express.json({ limit: '5mb' }))
